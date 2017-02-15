b126e8512041eeadffcf88c8a4283f96c6b4dc8d
mf <something@email.com>
2017-02-15 12:01:47 -0600

    added do/while loop

--- src/com/mark/adv_prob_1.java
+++ src/com/mark/adv_prob_1.java
@@ -3,6 +3,7 @@ package com.mark;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by hl4350hb on 2/15/2017.
@@ -10,25 +11,38 @@ import java.util.Random;
 public class adv_prob_1 {
 
     static Random random = new Random();
+    static Scanner stringScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ArrayList<String> cardDeck = makeDeck();
-        int randomIndex = random.nextInt(cardDeck.size());
-        String randomCard = cardDeck.get(randomIndex);
-        System.out.println(randomCard);
+        ArrayList<String[]> cardDeck = makeDeck();
+        String input = "";
+        do {
+            int randomIndex = random.nextInt(cardDeck.size());
+            String[] randomCard = cardDeck.get(randomIndex);
+            System.out.print("Random card: ");
+            System.out.println(randomCard[0] + " of " + randomCard[1]);
+            System.out.println("Press 'Y' to draw another random card.");
+            input = stringScanner.nextLine();
+        }
+            while (input.equalsIgnoreCase("Y"));
+        }
     }
 
 
 
     public static ArrayList makeDeck() {
         ArrayList newDeck = new ArrayList();
-        String[] newCard = new String[2];
+
         String[] allSuites = {"spades", "hearts", "diamonds", "clubs"};
         String[] allCards = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
         for (String card : allCards) {
             for (String suite : allSuites) {
-                newDeck.add(card + " of " + suite);
+                // Reinitializing each time so it is a new object.
+                String[] newCard = new String[2];
+                newCard[0] = card;
+                newCard[1] = suite;
+                newDeck.add(newCard);
             }
         }
         for (int x = 0; x < 5; x++) {
