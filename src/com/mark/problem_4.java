8c22adf734cb7510cefeb626364099598b9d12da
mf <something@email.com>
2017-02-16 16:12:10 -0600

    incorporated hashmap containing an arraylist

--- src/com/mark/problem_4.java
+++ src/com/mark/problem_4.java
@@ -1,9 +1,6 @@
 package com.mark;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by hl4350hb on 2/8/2017.
@@ -12,24 +9,44 @@ public class problem_4 {
     static Scanner numberScanner = new Scanner(System.in);
     static Scanner stringScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        ArrayList<ArrayList> lakeTimes = new ArrayList<ArrayList>();
+        HashMap<String, ArrayList> lakeTimesHash = new HashMap();
+
+
         System.out.println("What lake did you run?");
         String lake = stringScanner.nextLine();
         System.out.println("What was your time?");
         double time = numberScanner.nextDouble();
-        ArrayList newRun = new ArrayList();
-        newRun.add(lake);
-        newRun.add(time);
-        lakeTimes.add(newRun);
-        for (int x = 0; x < lakeTimes.size(); x++) {
-            ArrayList item = lakeTimes.get(x);
-            if (item.get(0).equals(lake)) {
-                System.out.println("Success!");
-            }
+
+        if (lakeTimesHash.containsKey(lake)) {
+            lakeTimesHash.get(lake).add(time);
         }
+        else {
+            ArrayList<Double> lakeTimesArray = new ArrayList<Double>();
+            lakeTimesArray.add(time);
+            lakeTimesHash.put(lake, lakeTimesArray);
+        }
+
+//        for (String key : lakesAndTimes.keySet()) {
+//            if (lake.equalsIgnoreCase(key)) {
+//
+//            }
+//        }
+
+//        ArrayList newRun = new ArrayList();
+//        newRun.add(lake);
+//        newRun.add(time);
+//        lakeTimes.add(newRun);
+//        for (int x = 0; x < lakeTimes.size(); x++) {
+//            ArrayList item = lakeTimes.get(x);
+//            if (item.get(0).equals(lake)) {
+//                System.out.println("Success!");
+//            }
+//        }
 
 
     }
 }
 // arraylist containing arrays[lake name, time]
+// NO!
+// dictionary containing ONE arraylist of times; key is lake
 // Arrays.sort(array)
