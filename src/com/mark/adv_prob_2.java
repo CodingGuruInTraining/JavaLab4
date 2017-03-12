a40aeb07ab6a0e180b4f6c36514afef3a2be06f5
MF <notsure@compuserv.com>
2017-03-11 20:26:17 -0600

    Added comments.

--- src/com/mark/adv_prob_2.java
+++ src/com/mark/adv_prob_2.java
@@ -16,25 +16,31 @@ public class adv_prob_2 {
         int cards_per_Player = 5;
         int player_Count = 2;
 
+        // Creates HashMap that will hold all the player objects and their
+        // card objects.
         HashMap all_players_cards = new HashMap();
 
+        // Runs function to generate a deck of cards.
         ArrayList<String[]> cardDeck = makeDeck();
 
+        // Loops through the number of players and draws 5 random cards
+        // from deck.
         for (int x = 1; x <= player_Count; x++) {
             ArrayList<String[]> player_hand = dealCardsToHand(cards_per_Player, cardDeck);
             all_players_cards.put(x, player_hand);
 
-
-
+            // Displays all cards in the player's hand.
             String player_hand_string = makeHandString(player_hand);
+            System.out.println("Player #" + x + ":");
             System.out.println(player_hand_string);
-//        // Displays all cards in the player's hand.
-//        System.out.println(Arrays.toString(player_hand));
-//        // Prompts user for card number they are looking for
-//        System.out.println("What cards are you looking for? (1-13)");
-//        int wanted_cards = numberScanner.nextInt();
-//        // Displays generic response.
-//        System.out.println("I don't have any of those. GO FISH!");
+
+            // Prompts user for card number they are looking for
+            System.out.println("What cards are you looking for? (1-13)");
+            int wanted_cards = numberScanner.nextInt();
+            // Displays generic response.
+            System.out.println("I don't have any of those. GO FISH!");
+
+            //      to be continued...
         }
     }
 
@@ -45,7 +51,8 @@ public class adv_prob_2 {
         ArrayList newDeck = new ArrayList();
         // Creates static arrays of each suite name and each card type.
         String[] allSuites = {"spades", "hearts", "diamonds", "clubs"};
-        String[] allCards = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        String[] allCards = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King"};
         // Loops through both arrays and creates a new array
         // with a unique combination.
         for (String card : allCards) {
@@ -76,6 +83,7 @@ public class adv_prob_2 {
         return aCard;
     }
 
+    // Initial cards are drawn and placed in ArrayList.
     public static ArrayList dealCardsToHand(int cards_per_Player, ArrayList cardDeck) {
         ArrayList<String[]> player_hand = new ArrayList<String[]>();
         for (int i = 0; i < cards_per_Player; i++) {
@@ -85,11 +93,14 @@ public class adv_prob_2 {
         return player_hand;
     }
 
+    // Simple function that makes a printable list of cards.
     public static String makeHandString(ArrayList<String[]> player_hand) {
         String hand = "";
+        // Loops through cards and combines values in array.
         for (String[] card : player_hand) {
             hand += card[0] + " " + card[1] + ", ";
         }
+        // Removes trailing separator.
         hand = hand.substring(0, hand.length() - 2);
         return hand;
     }
