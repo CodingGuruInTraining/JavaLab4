e0033feedb59da80be963f0f730de556ce0a7ac2
MF <notsure@compuserv.com>
2017-03-11 19:51:45 -0600

    Cleared problem_3 and added link to its code, which is in another repository. Finished problem_4 and added comments.

--- src/com/mark/problem_4.java
+++ src/com/mark/problem_4.java
@@ -1,57 +1,68 @@
 package com.mark;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
- * Created by hl4350hb on 2/8/2017.
+ * This program prompts user for inputs and adds these values
+ * to a HashMap.
  */
 public class problem_4 {
+    // Creates Scanner objects.
     static Scanner numberScanner = new Scanner(System.in);
     static Scanner stringScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        // Creates HashMap object.
         HashMap<String, ArrayList> lakeTimesHash = new HashMap();
 
-
+        // Displays prompt for User and captures inputs
+        // in separate variables.
         System.out.println("What lake did you run?");
         String lake = stringScanner.nextLine();
         System.out.println("What was your time?");
         double time = numberScanner.nextDouble();
 
-//        if (lakeTimesHash.containsKey(lake)) {
-//            lakeTimesHash.get(lake).add(time);
-//        }
-//        else {
-//            ArrayList<Double> lakeTimesArray = new ArrayList<Double>();
-//            lakeTimesArray.add(time);
-//            lakeTimesHash.put(lake, lakeTimesArray);
-//        }
+        // Runs function that adds inputs to HashMap.
+        addToList(lakeTimesHash, lake, time);
 
+        // Displays message and 'column headers' for results.
         System.out.println("Your fastest times have been updated:");
         System.out.println("LAKE\tTIME");
 
+        // Loops through HashMap and displays lowest value.
         for (String key : lakeTimesHash.keySet()) {
-            double max = findMax(lakeTimesHash.get(key));
-            printMax(key, max);
+            double min = findMin(lakeTimesHash.get(key));
+            printMax(key, min);
         }
-
     }
 
+    // Function that adds parameters to HashMap.
     public static void addToList(HashMap hash, String input, double time) {
+        // Checks if ArrayList exists with same key.
         if (hash.containsKey(input)) {
-            hash.get(input).add(time);
+            // Adds time to ArrayList.
+            ArrayList<Double> lakeList = (ArrayList)hash.get(input);
+            lakeList.add(time);
         }
         else {
+            // Creates new ArrayList if one does not exist.
             ArrayList<Double> lakeTimesArray = new ArrayList<Double>();
             lakeTimesArray.add(time);
+            // Adds ArrayList to HashMap.
             hash.put(input, lakeTimesArray);
         }
     }
 
-    public static double findMax(ArrayList<Double> aList) {
-        return Collections.max(aList);
+    // Function that finds the minimum value in ArrayList.
+    public static double findMin(ArrayList<Double> aList) {
+        return Collections.min(aList);
     }
 
-    public static void printMax(String key, double max) {
-        System.out.println(key + " : " + max);
+    // Function that displays results.
+    public static void printMax(String key, double min) {
+        System.out.println(key + " : " + min);
     }
 }
